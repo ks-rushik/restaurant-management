f4eeccde965cbd4d303bc41e0e3ed36ef7ccaba7
Rushikatrodiya <rushi.katrodiya@kalkani.in>
2025-03-02 11:39:46 +0530

    form component created

---
+++ src/app/components/forms/FormField.tsx
@@ -0,0 +1,31 @@
+import { FC, ReactNode } from "react";
+import BaseLabel from "../ui/BaseLabel";
+import BaseErrorMessage from "../ui/BaseErrorMessage";
+
+type IFormFieldProps = {
+  label: string;
+  name?: string;
+  error?: string;
+  children?: ReactNode;
+  required?:boolean
+};
+
+const FormField: FC<IFormFieldProps> = (props) => {
+  const {
+    label,
+    name,
+    error,
+    children,
+    required
+  } = props;
+
+  return (
+    <div className="mb-6">
+      <BaseLabel htmlFor={name} labeltitle={label} required={required}/>
+      {children}
+      {error && <BaseErrorMessage error={new Error(error)} />}
+    </div>
+  );
+};
+
+export default FormField;
