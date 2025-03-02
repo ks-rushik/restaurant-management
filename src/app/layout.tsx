9ec10a63bdbd33ef9d6bfaec2be0fe3e78385334
Rushikatrodiya <rushi.katrodiya@kalkani.in>
2025-03-02 10:18:52 +0530

    dependecies added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -25,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <MantineProvider>{children}</MantineProvider>
       </body>
     </html>
   );
