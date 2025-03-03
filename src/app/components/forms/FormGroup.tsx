6e19f07a702284f4f08e53216b8858e00f36c993
ks-rushik <rushi.katrodiya@kalkani.in>
2025-03-03 16:31:56 +0530

    css change for formgroup

--- src/app/components/forms/FormGroup.tsx
+++ src/app/components/forms/FormGroup.tsx
@@ -7,7 +7,7 @@ type IFormGroupProps = {
 const FormGroup: FC<IFormGroupProps> = ({ children }) => {
   return (
     <div className="flex min-h-screen items-center justify-center px-4 py-6 sm:px-6 lg:px-8 bg-gray-50">
-      <div className="w-full max-w-md p-8 bg-white dark:bg-gray-800 rounded-xl shadow-2xl border dark:border-gray-700">
+      <div className="min-w-[500px] max-w-md p-8 bg-white dark:bg-gray-800 rounded-xl shadow-2xl border dark:border-gray-700">
         {children}
       </div>
     </div>
