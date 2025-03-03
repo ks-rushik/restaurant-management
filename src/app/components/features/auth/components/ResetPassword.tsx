8dd27eb92e04b393e3e4ff42c2b6be1540e360e1
ks-rushik <rushi.katrodiya@kalkani.in>
2025-03-03 16:29:26 +0530

    show description for reset link form

--- src/app/components/features/auth/components/ResetPassword.tsx
+++ src/app/components/features/auth/components/ResetPassword.tsx
@@ -8,11 +8,10 @@ import FormField from "@/app/components/forms/FormField";
 import BaseInput from "@/app/components/ui/BaseInput";
 import BaseButton from "@/app/components/ui/BaseButton";
 import { resetPassword } from "../actions/resetpassword-action";
+import { Text } from "@mantine/core";
 
 const resetPasswordSchema = z.object({
-  email: z.string()
-    .min(1, "Email is Required")
-    .email("Invalid email format"),
+  email: z.string().min(1, "Email is Required").email("Invalid email format"),
 });
 
 export type IResetPasswordData = z.infer<typeof resetPasswordSchema>;
@@ -28,36 +27,43 @@ const ResetPasswordForm = () => {
 
   const onSubmit = (data: IResetPasswordData) => {
     console.log("Login Data:", data);
-    return resetPassword(data.email)
+    return resetPassword(data.email);
   };
 
   return (
     <>
-      <form onSubmit={handleSubmit(onSubmit)} className="">
-        <FormGroup>
+      <form onSubmit={handleSubmit(onSubmit)} className="w-full">
+        <FormGroup >
           <h1 className="flex flex-col items-center justify-center mb-10 text-xl">
             Password Recovery
           </h1>
-          <FormField label="Email" name="email" error={errors.email?.message} required={true}>
+          <Text size="md" className="pt-2 pr-2 pb-8">
+            Enter your registered email address. We'll send you a link or code to reset your password.
+          </Text>
+
+          <FormField
+            label="Email"
+            name="email"
+            error={errors.email?.message}
+            required={true}
+          >
             <BaseInput
               {...register("email")}
               type="email"
               name="email"
               placeholder="Enter your email..."
-             
             />
           </FormField>
           <BaseButton
             type="submit"
-            intent="success"
+            intent="primary"
             classNames={{
               root: "mb-2 w-full py-2 rounded-md",
             }}
-            loading={isSubmitting} 
+            loading={isSubmitting}
           >
-          Enter Email
+            Enter Email
           </BaseButton>
-        
         </FormGroup>
       </form>
     </>
