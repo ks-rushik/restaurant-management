a99fb6d4056ba09daa67cd23321d81f109479911
ks-rushik <rushi.katrodiya@kalkani.in>
2025-03-04 16:46:09 +0530

    use menu of mantine instead of usestate

--- src/app/features/navbar/Navbar.tsx
+++ src/app/features/navbar/Navbar.tsx
@@ -2,44 +2,45 @@
 
 import { useState } from "react";
 import Link from "next/link";
-import { Avatar } from "@mantine/core";
+import { Avatar, Menu } from "@mantine/core";
 import Image from "next/image";
 import website from "./website.svg";
 import LogOut from "../auth/components/Logout";
 
 const Navbar = () => {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [opened, setOpened] = useState(false);
 
   return (
     <nav className="bg-[#fff] border-gray-200 mt-2 text-white border-b-2 border-b-gray-150 flex justify-between items-center shadow-lg">
       <div className="flex items-center ">
         <Image src={website} width={100} height={100} alt="Website Logo" />
       </div>
+      <Menu
+        opened={opened}
+        onChange={setOpened}
+        zIndex={0}
+        classNames={{ item: "font-[650]" }}
+      >
+        <Menu.Target>
+          <Avatar
+            src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-1.png"
+            alt="User Avatar"
+            size={"lg"}
+            classNames={{
+              root: "transition delay-100 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 xl:mr-1",
+            }}
+          />
+        </Menu.Target>
 
-      <div className="relative">
-        <Avatar
-          src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-1.png"
-          alt="User Avatar"
-          size={"lg"}
-          onClick={() => setDropdownOpen(!dropdownOpen)}
-          classNames={{
-            root: "transition delay-100 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 xl:mr-1",
-          }}
-        />
-
-        {dropdownOpen && (
-          <div className="absolute right-0 mt-2 w-40  text-black shadow-lg bg-blue-50 z-20 rounded-xl">
-            <Link
-              href="/userprofile"
-              className="block px-4 py-2"
-              onClick={() => setDropdownOpen(false)}
-            >
+        <Menu.Dropdown>
+          <Menu.Item classNames={{ item: "hover:bg-gray-200" }}>
+            <Link href="/userprofile" className="block px-4 py-2">
               Profile Page
             </Link>
-            <LogOut />
-          </div>
-        )}
-      </div>
+          </Menu.Item>
+          <Menu.Item component={LogOut}>LogOut</Menu.Item>
+        </Menu.Dropdown>
+      </Menu>
     </nav>
   );
 };
