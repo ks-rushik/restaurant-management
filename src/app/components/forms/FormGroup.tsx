b7f205b06f615a1c291bf183621e22ab881c7f46
ks-rushik <rushi.katrodiya@kalkani.in>
2025-03-04 18:34:29 +0530

    make responsive forms and some changes in login and logout component regarding css

--- src/app/components/forms/FormGroup.tsx
+++ src/app/components/forms/FormGroup.tsx
@@ -1,3 +1,4 @@
+import { BackgroundImage, Box, Center } from "@mantine/core";
 import { FC, ReactNode } from "react";
 
 type IFormGroupProps = {
@@ -7,7 +8,7 @@ type IFormGroupProps = {
 const FormGroup: FC<IFormGroupProps> = ({ children }) => {
   return (
     <div className="flex min-h-screen items-center justify-center px-4 py-6 sm:px-6 lg:px-8 bg-gray-50">
-      <div className="min-w-[500px] max-w-md p-8 bg-white dark:bg-gray-800 rounded-xl shadow-2xl border dark:border-gray-700">
+      <div className="w-full max-w-md p-6 sm:p-8 bg-white  rounded-xl shadow-2xl border">
         {children}
       </div>
     </div>
--- src/app/features/auth/components/Logout.tsx
+++ src/app/features/auth/components/Logout.tsx
@@ -2,24 +2,28 @@ import { useDisclosure } from "@mantine/hooks";
 import { Modal } from "@mantine/core";
 import BaseButton from "@/app/components/ui/BaseButton";
 import signout from "../actions/logout-action";
+import { useState } from "react";
 
 const LogOut = () => {
   const [opened, { open, close }] = useDisclosure(false);
+  const [loading, setLoading] = useState(false);
+
+  const handlelogout = () => {
+    setLoading(true);
+    signout();
+  };
   return (
     <>
-      <Modal opened={opened} onClose={close} title="Confirm action" size={"md"}  >
+      <Modal opened={opened} onClose={close} title="Confirm action" size={"md"}>
         <div className="flex flex-col">
           Are you sure you want to logout?
           <BaseButton
             type="submit"
             intent={"primary"}
-            classNames={{root:"w-32 mt-4"}}
-            onClick={() => {
-              signout();
-              close();
-            }}
+            classNames={{ root: "w-32 mt-4" }}
+            onClick={() => handlelogout()}
           >
-            Logout
+            {loading ? <p>Loading...</p> : <p>Logout</p>}
           </BaseButton>
         </div>
       </Modal>
