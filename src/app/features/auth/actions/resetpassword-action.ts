38370ee4ac5ce7603d4673cacfab6a201bd454dc
ks-rushik <rushi.katrodiya@kalkani.in>
2025-03-06 10:09:29 +0530

    resetpassword-action functionality changed instead of redirect manually changed in supabase

--- src/app/features/auth/actions/resetpassword-action.ts
+++ src/app/features/auth/actions/resetpassword-action.ts
@@ -8,14 +8,13 @@ import { redirect } from "next/navigation";
 export async function resetPassword(email: string ) {
   const supabase = await createClient();
 
-  const { error } = await supabase.auth.resetPasswordForEmail(email, {
-    redirectTo: "http://localhost:3000/auth/updatepassword",
-  });
+  const { data , error } = await supabase.auth.resetPasswordForEmail(email);
 
   if (error) {
-    redirect("/error");
+    redirect("/auth/error");
   }
-
+ console.log(error);
+ 
   revalidatePath("/", "layout");
-  redirect("/auth/login");
+  return{ message: "link sent to this email"}
 }
