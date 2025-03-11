57b0b7bf7ad9c09dd7f4f18102b5fab554120cd6
ks-rushik <rushi.katrodiya@kalkani.in>
2025-03-11 18:29:11 +0530

    layout css added and pagefile import changed

--- src/app/(dashboard)/menu/page.tsx
+++ src/app/(dashboard)/menu/page.tsx
@@ -1,18 +1,22 @@
 import fetchMenudata from "@/app/features/menu/actions/menu-fetch";
-import Menu from "@/app/features/menu/components/Menu";
-import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
+import Menu from "@/app/features/menu/components/MenuPage";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 
 export const queryClient = new QueryClient();
-const Menupage = async() => {
+const Menupage = async () => {
   await queryClient.prefetchQuery({
-    queryKey:['menu'],
-    queryFn: fetchMenudata
-  })
+    queryKey: ["menu"],
+    queryFn: fetchMenudata,
+  });
 
-  return(
+  return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <Menu/>
+      <Menu />
     </HydrationBoundary>
-    )
-}
-export default Menupage
+  );
+};
+export default Menupage;
