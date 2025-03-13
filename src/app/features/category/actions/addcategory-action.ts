2601c17f9c57aab5520d9226604fbbf1c3b0a3af
ks-rushik <rushi.katrodiya@kalkani.in>
2025-03-13 18:09:24 +0530

    addcategory-action created and fetch the data

--- src/app/features/category/actions/addcategory-action.ts
+++ src/app/features/category/actions/addcategory-action.ts
@@ -4,17 +4,12 @@ import { createClient } from "@/app/utils/supabase/server";
 import { revalidatePath } from "next/cache";
 import { ICategorydata } from "../components/AddCategoryModal";
 
-const categories = async (CategoryData: ICategorydata) => {
+const categories = async (CategoryData: ICategorydata , menuId : string) => {
   const supabase = await createClient();
-
-  const { data: menuData } = await supabase
-    .from("menus")
-    .select("id")
-    .limit(1)
-    .single();
   const categorydata = {
-    menu_id: menuData?.id,
+    menu_id: menuId,
     category_name: CategoryData.category_name,
+    status:CategoryData.status
   };
 
   const { data: InsertData, error } = await supabase
@@ -25,8 +20,7 @@ const categories = async (CategoryData: ICategorydata) => {
   if (error) {
     console.log(error);
   }
-  console.log(InsertData);
-  
+
 
   revalidatePath("/", "layout");
 
--- src/app/features/category/actions/category-fetch.ts
+++ src/app/features/category/actions/category-fetch.ts
@@ -1,20 +1,13 @@
 "use server";
 import { createClient } from "@/app/utils/supabase/server";
 
-const fetchCategorydata = async () => {
-  const supabase = await createClient();
-  const { data: menuData } = await supabase
-  .from("menus")
-  .select("id")
-  .limit(1)  
-  .single();
-  
-
+const fetchCategorydata = async (menuId : string) => {
+  const supabase = await createClient();  
   const { data ,error } = await supabase
     .from("category")
     .select("*")
-    .eq("menu_id", menuData?.id)
-    .order("create_at", { ascending: true });
+    .eq("menu_id", menuId)
+    .order("create_at", { ascending: true } );
   
   return data;
 };
