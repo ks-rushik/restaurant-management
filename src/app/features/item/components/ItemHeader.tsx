0ef22bb9c82a3a0a7e8f5e4ce57f1184a00c02ed
ks-rushik <rushi.katrodiya@kalkani.in>
2025-03-17 18:01:35 +0530

    changed title of addcategorymodal

--- src/app/features/item/components/ItemHeader.tsx
+++ src/app/features/item/components/ItemHeader.tsx
@@ -1,28 +1,32 @@
 "use client";
 import CustomBreadcrumbs from "@/app/components/ui/BaseBreadcrumbs";
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { FC, ReactNode } from "react";
 
 type ICategoryHeaderProps = {
   children: ReactNode;
 };
-const  ItemHeader: FC<ICategoryHeaderProps> = (props) => {
+const ItemHeader: FC<ICategoryHeaderProps> = (props) => {
   const { children } = props;
   const searchParam = useSearchParams();
-  const menuname = searchParam.get("name")!;
+  const name = searchParam.get("name")!;
+  const menuname = searchParam.get("menuname")!;
   const pathname = usePathname();
 
   const segments = pathname.split("/")[1];
-  console.log(menuname.split("/")[0] ,"item");
-
-
+  const menuId = pathname.split('/')[2];
+  
   const breadcrumbItems = [
     {
       title: segments[0].toUpperCase() + segments.slice(1),
       href: `/${segments}`,
     },
     {
-      title: menuname[0].toUpperCase() + menuname?.slice(1),
+        title: menuname[0].toUpperCase() + menuname.slice(1),
+        href:`/menu/${menuId}?name=${menuname}`
+    },
+    {
+      title: name[0].toUpperCase() + name?.slice(1),
       href: `#`,
     },
   ];
