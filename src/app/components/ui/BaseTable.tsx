df28633037481ba4e74fb3a7a73d542a81b27321
ks-rushik <rushi.katrodiya@kalkani.in>
2025-03-17 17:58:12 +0530

    css changed of basetable for description

--- src/app/components/ui/BaseTable.tsx
+++ src/app/components/ui/BaseTable.tsx
@@ -6,6 +6,7 @@ import BaseButton from "./BaseButton";
 type IColumn<T> = {
   label: string;
   render: (item: T) => ReactNode;
+  width?: string;
 };
 
 type IBaseTableProps<T> = TableProps & {
@@ -16,7 +17,6 @@ type IBaseTableProps<T> = TableProps & {
   initialSize?: number;
   loadMoreSize?: number;
 };
-
 const BaseTable = <T,>({
   classNames,
   columns,
@@ -35,47 +35,72 @@ const BaseTable = <T,>({
 
   return (
     <>
-    <div className="flex items-center w-full overflow-hidden rounded-2xl border-2 border-gray-200 bg-white">
-      <div className="w-full overflow-x-auto">
-        <Table
-          classNames={{
-            table: clsx("w-full min-w-[600px]", table),
-            thead: clsx("text-bold p-8 ", thead),
-            th: clsx("text-gray-600 text-sm h-12 font-bold ", th),
-            td: clsx("text-gray-500 text-sm h-12 font-semibold  ", td),
-            ...otherelements,
-          }}
-          {...other}
-        >
-          <Table.Thead>
-            <Table.Tr>
-              {columns.map((col, index) => (
-                <Table.Th key={index}>{col.label}</Table.Th>
-              ))}
-            </Table.Tr>
-          </Table.Thead>
-          <Table.Tbody>
-            {data?.slice(0, visibleCount).map((row) => (
-              <Table.Tr
-                key={getKey(row)}
-            
-                className="cursor-pointer hover:bg-gray-100 transition"
-              >
+      <div className="w-full overflow-hidden rounded-2xl border-2 border-gray-200 bg-white">
+        <div className="w-full overflow-x-auto md:overflow-hidden">
+          <Table
+            classNames={{
+              table: clsx(
+                "w-full min-w-full md:min-w-[600px] table-auto",
+                table
+              ),
+              thead: clsx("text-bold p-8", thead),
+              th: clsx("text-gray-600 text-sm h-12 font-bold px-4", th),
+              td: clsx("text-gray-500 text-sm h-12 font-semibold px-4", td),
+              ...otherelements,
+            }}
+            {...other}
+          >
+            <Table.Thead>
+              <Table.Tr>
                 {columns.map((col, index) => (
-                  <Table.Td key={index}>{col.render(row)}</Table.Td>
+                  <Table.Th
+                    key={index}
+                    className={clsx(
+                      col.width
+                        ? `w-[${col.width}] md:min-w-[${col.width}]`
+                        : "w-full md:w-auto",
+                      "p-3"
+                    )}
+                  >
+                    {col.label}
+                  </Table.Th>
                 ))}
               </Table.Tr>
-            ))}
-          </Table.Tbody>
-        </Table>
+            </Table.Thead>
+            <Table.Tbody>
+              {data?.slice(0, visibleCount).map((row) => (
+                <Table.Tr
+                  key={getKey(row)}
+                  className="cursor-pointer hover:bg-gray-100 transition"
+                >
+                  {columns.map((col, index) => (
+                    <Table.Td
+                      key={index}
+                      className={clsx(
+                        col.width
+                          ? `w-[${col.width}] md:min-w-[${col.width}]`
+                          : "w-full md:w-auto",
+                        col.label === "DESCRIPTION"
+                          ? "break-words whitespace-normal line-clamp-3 md:line-clamp-1"
+                          : "",
+                        "p-3"
+                      )}
+                    >
+                      {col.render(row)}
+                    </Table.Td>
+                  ))}
+                </Table.Tr>
+              ))}
+            </Table.Tbody>
+          </Table>
+        </div>
       </div>
-     
-    </div>
-    { visibleCount < data!.length && (
-      <div className="flex justify-center">
-        <BaseButton onClick={handleLoadMore} className="mt-4 ">
-          Load More
-        </BaseButton>
+
+      {visibleCount < data!.length && (
+        <div className="flex justify-center">
+          <BaseButton onClick={handleLoadMore} className="mt-4">
+            Load More
+          </BaseButton>
         </div>
       )}
     </>
