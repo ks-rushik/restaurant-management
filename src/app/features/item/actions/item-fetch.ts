ed0047828af3d32bad0e5e039ce47edc8c8b0dfe
ks-rushik <rushi.katrodiya@kalkani.in>
2025-03-17 17:57:22 +0530

    price fetched from menus table

--- src/app/features/item/actions/item-fetch.ts
+++ src/app/features/item/actions/item-fetch.ts
@@ -1,13 +1,18 @@
 "use server";
 import { createClient } from "@/app/utils/supabase/server";
+import { fetchMenus } from "./sample-action";
+
 const fetchItemdata = async (categoryId: string) => {
   const supabase = await createClient();
   const { data, error } = await supabase
     .from("Items")
-    .select("*")
+    .select("*, category:category_id(menu:menu_id!inner(currency))")
     .eq("category_id", categoryId)
-    .order("position", { ascending: true }); 
+    .order("position", { ascending: true });
+
   if (error) throw error;
   return data;
 };
+
+fetchMenus();
 export default fetchItemdata;
