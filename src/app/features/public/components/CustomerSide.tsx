b5f4f27455f2d89cce8211d537c36d1baa97da67
ks-rushik <rushi.katrodiya@kalkani.in>
2025-03-19 17:44:43 +0530

    change css in customersidefile

--- src/app/features/public/components/CustomerSide.tsx
+++ src/app/features/public/components/CustomerSide.tsx
@@ -1,18 +1,20 @@
 "use client";
 import CustomerSideHeader from "./CustomerSideHeader";
 import useCategoriesItems from "../hook/useCategoriesItems";
-
 import CustomerSideBody from "./CustomerSideBody";
+
 
 const CustomerSide = ({ id }: { id: string }) => {
   const { categories } = useCategoriesItems(id) || {};
 
-
   return (
-    <div className="min-h-screen bg-gradient-to-b from-gray-100 to-gray-200 px-4 sm:px-12 md:px-16 lg:px-20 xl:px-32 py-6">
-      <CustomerSideHeader />
-      <CustomerSideBody categories={categories} id={id}/>
+    <div className="flex flex-col lg:flex-row lg:items-start container mx-auto mb-10">
+      <div className=" w-full">
+        <CustomerSideHeader />
+        <CustomerSideBody categories={categories} id={id} />
+      </div>
     </div>
   );
 };
+
 export default CustomerSide;
