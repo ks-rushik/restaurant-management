24687effec8cf57e6796a5d542e56429dfde1367
ks-rushik <rushi.katrodiya@kalkani.in>
2025-03-26 18:35:16 +0530

    formating file

--- src/app/components/customer/CustomerSide.tsx
+++ src/app/components/customer/CustomerSide.tsx
@@ -8,13 +8,15 @@ const CustomerSide = ({ id }: { id: string }) => {
   const menuid = useShortUrl(id);
   const dataId = menuid?.[0].menu_id;
 
-  const { categories } = useCategoriesItems(dataId) || {};
+  const { categories ,userEmail } = useCategoriesItems(dataId) || {};
+  console.log(userEmail);
+  
 
   const profilelogo = categories?.[0]?.menus.restaurant_id.logo;
   const profileName = categories?.[0]?.menus.restaurant_id.name;
 
   return (
-    <div className="container px-6 mx-auto  mb-10">
+    <div className="container px-4 mx-auto  mb-10">
       <CustomerSideHeader logo={profilelogo} name={profileName} />
       <CustomerSideBody categories={categories} id={id} />
     </div>
