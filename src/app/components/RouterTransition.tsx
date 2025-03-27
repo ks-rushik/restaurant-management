7a5da38b0d817609c8031cd9b7b6a4834eb389c2
ks-rushik <rushi.katrodiya@kalkani.in>
2025-03-27 18:36:10 +0530

    in global css add root color make themeprovider component

--- src/app/components/RouterTransition.tsx
+++ src/app/components/RouterTransition.tsx
@@ -1,20 +1,14 @@
 "use client";
 import { usePathname, useSearchParams } from "next/navigation";
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { NavigationProgress, nprogress } from "@mantine/nprogress";
 
 const RouterTransition = () => {
   const pathname = usePathname();
   const searchParams = useSearchParams();
-  const firstRender = useRef(true); 
 
   useEffect(() => {
-    if (firstRender.current) {
-      firstRender.current = false;
-      return;
-    }
-
-    nprogress.start(); 
+    nprogress.start();
     setTimeout(() => nprogress.complete(), 500);
 
     return () => {
