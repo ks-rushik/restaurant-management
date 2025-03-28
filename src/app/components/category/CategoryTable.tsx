def9870b833c3e43e5f52638b115f4b7534e1447
ks-rushik <rushi.katrodiya@kalkani.in>
2025-03-28 18:29:22 +0530

    Implement searchInput in categorypage

--- src/app/components/category/CategoryTable.tsx
+++ src/app/components/category/CategoryTable.tsx
@@ -1,15 +1,16 @@
 import BaseTable from "@/app/components/ui/BaseTable";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { ICategorydata } from "./AddCategoryModal";
 import CategoryActions from "./CategoryActions";
 import Loader from "@/app/components/ui/BaseLoader";
 import { FaDownLong, FaUpLong } from "react-icons/fa6";
 import formatDate from "@/app/utils/formatdate";
+import SearchInput from "../SearchInput";
 
 type ICategoryTableProps = {
   data: ICategorydata[] | undefined | null;
   handleSelectCategory: (item: ICategorydata) => void;
-  handleView: ( id: string) => void;
+  handleView: (id: string) => void;
   handleMoveUp: (index: number) => void;
   handleMoveDown: (index: number) => void;
   handleDelete: (
@@ -33,6 +34,14 @@ const CategoryTable: FC<ICategoryTableProps> = (props) => {
     opened,
     close,
   } = props;
+  const [searchData, setSearchData] = useState("");
+
+  console.log(data);
+  const filteredData = data
+    ? data.filter((item) =>
+        item?.category_name?.toLowerCase().includes(searchData.toLowerCase())
+      )
+    : [];
 
   return !data ? (
     <Loader></Loader>
@@ -41,77 +50,90 @@ const CategoryTable: FC<ICategoryTableProps> = (props) => {
       No Category available. Click "Add New Category" to create one.
     </p>
   ) : (
-    <BaseTable
-      classNames={{
-        th: "text-gray-600 text-sm h-12 font-bold [&:first-child]:w-[70px] ",
-        td: "text-gray-500 text-sm h-12 font-semibold [&:first-child]:w-[70px] ",
-      }}
-      data={data!}
-      getKey={(item) => item.id!}
-      columns={[
-        {
-          label: "",
-          render: (item) => {
-            const index = data!.findIndex(
-              (dataItem) => dataItem.id === item.id
-            );
-            return (
-              <div className="flex gap-1">
-                <button
-                  onClick={() => handleMoveUp(index)}
-                  disabled={index === 0}
-                  className="text-gray-500 hover:text-gray-700 disabled:opacity-50 dark:text-white dark:hover:text-gray-500"
-                >
-                  <FaUpLong />
-                </button>
-                <button
-                  onClick={() => handleMoveDown(index)}
-                  disabled={index === data!.length - 1}
-                  className="text-gray-500 hover:text-gray-700 disabled:opacity-50 dark:text-white dark:hover:text-gray-500"
-                >
-                  <FaDownLong />
-                </button>
-              </div>
-            );
-          },
-        },
-        {
-          label: "CATEGORY NAME",
-          render: (item) => item.category_name,
-        },
-        {
-          label: "AVAILABILITY",
-          render: (item) =>
-            item.status === "Not Available" ? (
-              <p className="text-red-500">Not Available</p>
-            ) : (
-              <p className="text-green-600">Available</p>
-            ),
-        },
-        {
-          label: "CREATED AT",
-          render: (item) => formatDate(item.create_at!),
-        },
-        {
-          label: "UPDATED AT",
-          render: (item) => formatDate(item.updated_at!),
-        },
-        {
-          label: "",
-          render: (item) => (
-            <CategoryActions
-              item={item}
-              handleView={handleView}
-              handleSelectCategory={handleSelectCategory}
-              handleDelete={handleDelete}
-              loading={loading}
-              opened={opened}
-              close={close}
-            />
-          ),
-        },
-      ]}
-    />
+    <>
+      <SearchInput
+        value={searchData}
+        onChange={(e) => setSearchData(e.target.value)}
+        placeholder="Search Category..."
+      />
+      {filteredData.length === 0 ? (
+        <p className="text-center text-gray-500 mt-4">
+          No matching categories found.
+        </p>
+      ) : (
+        <BaseTable
+          data={filteredData}
+          classNames={{
+            th: "text-gray-600 text-sm h-12 font-bold [&:first-child]:w-[70px] ",
+            td: "text-gray-500 text-sm h-12 font-semibold [&:first-child]:w-[70px] ",
+          }}
+          getKey={(item) => item.id!}
+          columns={[
+            {
+              label: "",
+              render: (item) => {
+                const index = data!.findIndex(
+                  (dataItem) => dataItem.id === item.id
+                );
+                return (
+                  <div className="flex gap-1">
+                    <button
+                      onClick={() => handleMoveUp(index)}
+                      disabled={index === 0}
+                      className="text-gray-500 hover:text-gray-700 disabled:opacity-50 dark:text-white dark:hover:text-gray-500"
+                    >
+                      <FaUpLong />
+                    </button>
+                    <button
+                      onClick={() => handleMoveDown(index)}
+                      disabled={index === data!.length - 1}
+                      className="text-gray-500 hover:text-gray-700 disabled:opacity-50 dark:text-white dark:hover:text-gray-500"
+                    >
+                      <FaDownLong />
+                    </button>
+                  </div>
+                );
+              },
+            },
+            {
+              label: "CATEGORY NAME",
+              render: (item) => item.category_name,
+            },
+            {
+              label: "AVAILABILITY",
+              render: (item) =>
+                item.status === "Not Available" ? (
+                  <p className="text-red-500">Not Available</p>
+                ) : (
+                  <p className="text-green-600">Available</p>
+                ),
+            },
+            {
+              label: "CREATED AT",
+              render: (item) => formatDate(item.create_at!),
+            },
+            {
+              label: "UPDATED AT",
+              render: (item) => formatDate(item.updated_at!),
+            },
+            {
+              label: "",
+              render: (item) => (
+                <CategoryActions
+                  item={item}
+                  handleView={handleView}
+                  handleSelectCategory={handleSelectCategory}
+                  handleDelete={handleDelete}
+                  loading={loading}
+                  opened={opened}
+                  close={close}
+                />
+              ),
+            },
+          ]}
+        />
+      )}
+    </>
   );
 };
 
