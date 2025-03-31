a31bec02422fb92a11a78ea6dd437394d1fe1340
ks-rushik <rushi.katrodiya@kalkani.in>
2025-03-31 13:40:18 +0530

    make filterdata component for filter data

---
+++ src/app/components/FilterData.tsx
@@ -0,0 +1,28 @@
+import { Select } from "@mantine/core";
+import { FC } from "react";
+import { LuFilter } from "react-icons/lu";
+
+type ISearchInputProps = {
+  value: string;
+  onChange: (value: string | null) => void;
+};
+const icon = <LuFilter size={20} />;
+
+const FilteredData: FC<ISearchInputProps> = (props) => {
+  const { value, onChange } = props;
+  return (
+    <Select
+      leftSectionPointerEvents="none"
+      leftSection={icon}
+      checkIconPosition="right"
+      variant="unstyled"
+      value={value}
+      onChange={onChange}
+      data={["Available", "Not Available", "All"]}
+      classNames={{ input: "h-10 dark:bg-gray-700 bg-white dark:text-white" , root:"w-[24%] border border-gray-200 dark:bg-gray-700 dark:border-gray-700" ,dropdown:"dark:text-blue-400 dark:border-gray-700 dark:bg-gray-700"   }}
+      searchable
+    />
+  );
+};
+
+export default FilteredData;
