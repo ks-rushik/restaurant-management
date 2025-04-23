7fc9bca917cf006456b069aee3e017cacb901e90
ks-rushik <rushi.katrodiya@kalkani.in>
2025-04-23 10:55:14 +0530

    add cursor pointer on all clickable things

--- src/app/components/FilterData.tsx
+++ src/app/components/FilterData.tsx
@@ -19,7 +19,11 @@ const FilteredData: FC<ISearchInputProps> = (props) => {
       value={value}
       onChange={onChange}
       data={["Available", "Not Available", "All"]}
-      classNames={{ input: "h-10 dark:bg-gray-700 bg-white dark:text-white" , root:"sm:w-full mt-2 md:mt-0 xl:mt-0 lg:mt-0 md:w-[24%] xl:w-[24%] lg:w-[24%] border border-gray-200 dark:bg-gray-700 dark:border-gray-700" ,dropdown:"dark:text-blue-400 dark:border-gray-700 dark:bg-gray-700"}}
+      classNames={{
+        input: "h-10 dark:bg-gray-700 bg-white dark:text-white cursor-pointer",
+        root: "sm:w-full mt-2 md:mt-0 xl:mt-0 lg:mt-0 md:w-[24%] xl:w-[24%] lg:w-[24%] border border-gray-200 dark:bg-gray-700 dark:border-gray-700",
+        dropdown: "dark:text-blue-400 dark:border-gray-700 dark:bg-gray-700",
+      }}
       searchable
     />
   );
--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { Avatar, Menu } from "@mantine/core";
 import Image from "next/image";
-import logo2 from "../../images/logo2.png";
+import logo3 from "../../images/logo3.png"
 import LogOut from "../auth/Logout";
 import { useState, useEffect } from "react";
 import ThemeButton from "../ui/ThemeButton";
@@ -32,10 +32,10 @@ const Navbar = () => {
   }, [theme]);
 
   return (
-    <nav className="bg-white dark:bg-neutral-950 shadow-lg py-2 px-4 flex justify-between items-center">
+    <nav className="bg-white dark:bg-black/90 shadow-lg py-2 px-4 flex justify-between items-center">
       <div className="flex items-center">
         <Image
-          src={logo2}
+          src={logo3}
           width={100}
           height={100}
           alt="Website Logo"
@@ -60,13 +60,13 @@ const Navbar = () => {
               alt="User Avatar"
               size={"lg"}
               classNames={{
-                root: "transition delay-100 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 xl:mr-1",
+                root: "transition cursor-pointer delay-100 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 xl:mr-1",
               }}
             />
           </Menu.Target>
 
-          <Menu.Dropdown style={{ zIndex: 20 }} className="dark:bg-black dark:border-black">
-            <Menu.Item classNames={{ item: "text-black dark:bg-gray-700 dark:text-white" }}>
+          <Menu.Dropdown style={{ zIndex: 20 }} className="dark:bg-[#131414] dark:border-black" classNames={{dropdown:'dark:!bg-gray-800'}}>
+            <Menu.Item classNames={{ item: "text-black dark:bg-gray-800 dark:text-white"  }}>
               <Link href="/userprofile" className="block px-3 py-1">Profile Page</Link>
             </Menu.Item>
             <Menu.Item component={LogOut}>LogOut</Menu.Item>
