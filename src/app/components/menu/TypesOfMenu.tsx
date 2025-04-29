f638cd62f833958fce059a5bbd5367fd69ed8084
ks-rushik <rushi.katrodiya@kalkani.in>
2025-04-29 18:00:24 +0530

    creteTypesof menu component for diffrent type of menus

--- src/app/components/menu/TypesOfMenu.tsx
+++ src/app/components/menu/TypesOfMenu.tsx
@@ -1,8 +1,12 @@
 import React, { FC, useState } from "react";
 import BaseModal from "../ui/BaseModal";
-import BaseButton from "../ui/BaseButton";
 import { IMenudata } from "@/app/type/type";
 import { RiShareLine } from "react-icons/ri";
+import ShareMenu from "./ShareMenu";
+import CategoryBasedMenu from "./CategoryBasedMenu";
+import itemmenu from "./../../images/item-based-menu.png";
+import categorymenu from "./../../images/category-based-menu.png";
+import Image from "next/image";
 
 type ITypesOfMenuProps = {
   item: IMenudata;
@@ -10,7 +14,8 @@ type ITypesOfMenuProps = {
 
 const TypesOfMenu: FC<ITypesOfMenuProps> = (props) => {
   const { item } = props;
-  const [shareModalOpen, setShareModalOpen] = useState(false);
+  const [shareModalOpen, setModalOpen] = useState(false);
+
   return (
     <>
       <button
@@ -21,17 +26,33 @@ const TypesOfMenu: FC<ITypesOfMenuProps> = (props) => {
         }}
         title="Share Menu"
       >
-        <RiShareLine size={22} onClick={() => setShareModalOpen(true)} className="mr-6 hover:text-blue-500" />
+        <RiShareLine
+          size={22}
+          onClick={() => setModalOpen(true)}
+          className="mr-6 hover:text-blue-500"
+        />
       </button>
       <BaseModal
         opened={shareModalOpen}
-        onClose={() => setShareModalOpen(false)}
+        onClose={() => setModalOpen(false)}
         title="Share Menu Link"
         centered
       >
-        <div className="flex justify-around py-10">
-          <BaseButton>Item Based Menu</BaseButton>
-          <BaseButton>Category Based Menu</BaseButton>
+        <div className="flex justify-around py-10 gap-4">
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src={itemmenu} width={300} height={300} alt="Item-menu" />
+            <ShareMenu item={item} />
+          </div>
+
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image
+              src={categorymenu}
+              width={300}
+              height={300}
+              alt="Category-menu"
+            />
+            <CategoryBasedMenu item={item} />
+          </div>
         </div>
       </BaseModal>
     </>
