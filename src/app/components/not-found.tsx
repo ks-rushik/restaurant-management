f24187334450b34d37f566343c94e65bc4da5743
ks-rushik <rushi.katrodiya@kalkani.in>
2025-05-01 18:30:34 +0530

    create not-found component that use in all not-found funtion group route vise

--- src/app/components/not-found.tsx
+++ src/app/components/not-found.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function NotFound() {
+const NotFound = () => {
   return (
     <div className="bg-gray-100 flex flex-col items-center justify-center min-h-screen p-4 text-center">
       <h1 className="text-[4rem] text-gray-800 mb-4">404</h1>
@@ -9,4 +9,6 @@ export default function NotFound() {
       </p>
     </div>
   );
-}
+};
+
+export default NotFound;
