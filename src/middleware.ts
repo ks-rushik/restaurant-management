c73a2b5aa941607316a86d09ffc7484e7521b33c
ks-rushik <rushi.katrodiya@kalkani.in>
2025-05-12 17:53:51 +0530

    Add middleaware for language support

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,49 @@
-import { type NextRequest } from 'next/server'
-import { updateSession } from './app/utils/supabase/middleware'
+import { NextResponse } from "next/server";
+import { type NextRequest } from "next/server";
+
+import Negotiator from "negotiator";
+
+import { updateSession } from "./app/utils/supabase/middleware";
+
+const locales = ["en", "hd", "sp"];
+
+function getLocale(request: NextRequest): string {
+  const negotiator = new Negotiator({
+    headers: {
+      "accept-language": request.headers.get("accept-language") || "",
+    },
+  });
+  const languages = negotiator.languages();
+  const defaultLocale = "en";
+
+  const matchedLocale =
+    languages.find((language: string) => locales.includes(language)) ||
+    defaultLocale;
+  return matchedLocale;
+}
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request)
+  await updateSession(request);
+
+  const { pathname } = request.nextUrl;
+
+  const pathnameHasLocale = locales.some(
+    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`,
+  );
+
+  if (pathnameHasLocale) {
+    return;
+  }
+
+  const locale = getLocale(request);
+
+  request.nextUrl.pathname = `/${locale}${pathname}`;
+
+  return NextResponse.redirect(request.nextUrl);
 }
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
-     */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
-}
+};
