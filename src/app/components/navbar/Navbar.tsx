cabaced580eb0280d4723b1a85589445b72ec371
ks-rushik <rushi.katrodiya@kalkani.in>
2025-05-12 17:58:16 +0530

    In navbar create handlelanguagechange funtion

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -2,7 +2,8 @@
 
 import Image from "next/legacy/image";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { useState } from "react";
 
 import ChangePassword from "@components/auth/ChangePassword";
 import LogOut from "@components/auth/Logout";
@@ -12,11 +13,37 @@ import { Avatar, Menu } from "@mantine/core";
 import { useThemeToggle } from "@/app/hook/useThemetoggle";
 import logo3 from "@/app/images/logo3.png";
 
+import BaseSelect from "../ui/BaseSelect";
+
+const languageMap: Record<string, string> = {
+  English: "en",
+  Hindi: "hd",
+  Spanish: "sp",
+};
+
 const Navbar = () => {
   const [opened, setOpened] = useState(false);
   const [modalopened, setModalOpened] = useState(false);
-
   const { theme, toggleTheme, mounted } = useThemeToggle();
+
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const handleLanguageChange = (value: string | null) => {
+    if (!value) return;
+
+    const locale = languageMap[value];
+    const segments = pathname.split("/").filter(Boolean);
+
+    if (["en", "hd", "sp"].includes(segments[0])) {
+      segments[0] = locale;
+    } else {
+      segments.unshift(locale);
+    }
+
+    const newPath = "/" + segments.join("/");
+    router.push(newPath);
+  };
 
   return (
     <nav className="bg-white dark:bg-black/90 shadow-lg sticky top-0 z-20 py-2 px-4 flex justify-between items-center">
@@ -31,6 +58,14 @@ const Navbar = () => {
       </div>
 
       <div className="flex flex-row items-center gap-4">
+        <BaseSelect
+          data={["English", "Hindi", "Spanish"]}
+          defaultValue="English"
+          size="small"
+          classNames={{ root: "w-28" }}
+          onChange={handleLanguageChange}
+        />
+
         {mounted && <ThemeButton theme={theme!} onChange={toggleTheme} />}
 
         <Menu
@@ -43,7 +78,7 @@ const Navbar = () => {
             <Avatar
               src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-1.png"
               alt="User Avatar"
-              size={"lg"}
+              size="lg"
               classNames={{
                 root: "transition cursor-pointer delay-100 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 xl:mr-1",
               }}
