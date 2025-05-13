cca5765f3df7b939f9ce32ca345b606ba1de8dcc
ks-rushik <rushi.katrodiya@kalkani.in>
2025-05-13 15:54:04 +0530

    Create languageselector component and use in navbar

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Langar } from "next/font/google";
 import Image from "next/legacy/image";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
@@ -15,6 +16,7 @@ import { IMessages } from "@/app/[locale]/messages";
 import { useThemeToggle } from "@/app/hook/useThemetoggle";
 import logo3 from "@/app/images/logo3.png";
 
+import LanguageSelector from "../auth/LanguageSelector";
 import BaseSelect from "../ui/BaseSelect";
 
 export type INavbarProps = {
@@ -26,39 +28,6 @@ const Navbar: FC<INavbarProps> = (props) => {
   const [opened, setOpened] = useState(false);
   const [modalopened, setModalOpened] = useState(false);
   const { theme, toggleTheme, mounted } = useThemeToggle();
-  const [selectedLanguage, setSelectedLanguage] = useState("English");
-
-  const pathname = usePathname();
-  const router = useRouter();
-
-  const localeList = ["en", "hd", "sp"];
-  const labelList = ["English", "Hindi", "Spanish"];
-
-  useEffect(() => {
-    const currentLocale = pathname.split("/")[1];
-    const index = localeList.indexOf(currentLocale);
-    setSelectedLanguage(index !== -1 ? labelList[index] : "English");
-  }, [pathname]);
-
-  const handleLanguageChange = (value: string | null) => {
-    if (!value) return;
-
-    const index = labelList.indexOf(value);
-    const newLocale = localeList[index];
-    setSelectedLanguage(value);
-
-    Cookies.set("preferred_locale", newLocale, { expires: 365 });
-
-    const segments = pathname.split("/").filter(Boolean);
-
-    if (localeList.includes(segments[0])) {
-      segments[0] = newLocale;
-    } else {
-      segments.unshift(newLocale);
-    }
-
-    router.push("/" + segments.join("/"));
-  };
 
   return (
     <nav className="bg-white dark:bg-black/90 shadow-lg sticky top-0 z-20 py-2 px-4 flex justify-between items-center">
@@ -73,15 +42,7 @@ const Navbar: FC<INavbarProps> = (props) => {
       </div>
 
       <div className="flex flex-row items-center gap-4">
-        <BaseSelect
-          data={["English", "Hindi", "Spanish"]}
-          defaultValue="English"
-          value={selectedLanguage}
-          size="small"
-          classNames={{ root: "w-28" }}
-          onChange={(value) => handleLanguageChange(value)}
-        />
-
+        <LanguageSelector />
         {mounted && <ThemeButton theme={theme!} onChange={toggleTheme} />}
 
         <Menu
