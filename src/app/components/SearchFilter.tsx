8eb8b0ab53ab42222c7b6f3c5f25033bb64fad63
ks-rushik <rushi.katrodiya@kalkani.in>
2025-05-14 18:40:36 +0530

    Change css in searchfilterwrapper and change name

--- src/app/components/SearchFilter.tsx
+++ src/app/components/SearchFilter.tsx
@@ -1,16 +1,14 @@
-import React, { FC, ReactNode } from 'react'
+import React, { FC, ReactNode } from "react";
 
-type ISearchFilterProps = {
-    children: ReactNode
-}
+type ISearchFilterWrapperProps = {
+  children: ReactNode;
+};
 
-const SearchFilter:FC<ISearchFilterProps> = (props) => {
-    const {children} = props
+const SearchFilterWrapper: FC<ISearchFilterWrapperProps> = (props) => {
+  const { children } = props;
   return (
-    <div className=" flex flex-col gap-4 mb-4 justify-end md:flex-row">
-      {children}
-    </div>
-  )
-}
+    <div className=" flex flex-row gap-4 mb-4 justify-end">{children}</div>
+  );
+};
 
-export default SearchFilter
+export default SearchFilterWrapper;
